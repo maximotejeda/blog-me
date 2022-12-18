c6468e2c2af0d4b248cbf6a5b90ddf99e2a48a60
Maximo Tejeda <Maximotejeda@gmail.com>
2022-12-18 12:24:31 -0400

    ADD: endpoints for Static server

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"blog-me/internal/assets"
 	"blog-me/internal/blog"
 	"net/http"
 )
 
 var br = blog.BlogRoute
+var asets = assets.AssetsRoute
 
 func main() {
 	http.Handle("/blog/", br)
+	http.Handle("/assets/", asets)
 	http.ListenAndServe(":8080", nil)
 }
