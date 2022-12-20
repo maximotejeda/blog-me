56ad85571139dc919ee9fd9c76a6794acb6f76e7
Maximo Tejeda <Maximotejeda@gmail.com>
2022-12-20 11:06:22 -0400

    EDIT: blog is now root,  server params  from make

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"blog-me/internal/assets"
 	"blog-me/internal/blog"
+	"log"
 	"net/http"
+	"os"
 )
 
-var br = blog.BlogRoute
-var asets = assets.AssetsRoute
+var (
+	br    = blog.BlogRoute
+	asets = assets.AssetsRoute
+	port  = os.Getenv("SERVERPORT")
+	addr  = os.Getenv("SERVERADDR")
+)
 
 func main() {
-	http.Handle("/blog/", br)
 	http.Handle("/assets/", asets)
-	http.ListenAndServe(":8080", nil)
+	http.Handle("/", br)
+	log.Fatal(http.ListenAndServe(addr+":"+port, nil))
 }
