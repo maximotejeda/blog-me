217c7118b612fc83c3e312ed6b19843c0ac418f2
Maximo Tejeda <Maximotejeda@gmail.com>
2023-01-16 18:57:22 -0400

    TODO: pending cache recent post a pair of hours

---
+++ internal/cache/cahe.go
@@ -0,0 +1,4 @@
+// Module to cache on memory a limited number of requests
+// lie the service only work on GET requests must be
+// Easy to implement an on memory module for cache request
+package cache
